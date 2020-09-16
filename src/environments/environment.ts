ce661bab0ed96d3f5deddb7a56221d7ce9d13e5b
nachomonllor <nachomonllorc@gmail.com>
2020-09-16 16:22:55 -0300

    instalo express

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCJwyVmDALuusfMiUFkeTnJHB8k-t4jl4E',
-    authDomain: 'monllorgame.firebaseapp.com',
-    databaseURL: 'https://monllorgame.firebaseio.com',
-    projectId: 'monllorgame',
-    storageBucket: 'monllorgame.appspot.com',
-    messagingSenderId: '676008548347',
-    appId: '1:676008548347:web:5849cb651142caf277dd5e'
+    apiKey: "AIzaSyDCfTu3FPPuQsbeqU4eJ2hVoAYLJ0a1SRA",
+    authDomain: "saladejuegos-2c3af.firebaseapp.com",
+    databaseURL: "https://saladejuegos-2c3af.firebaseio.com",
+    projectId: "saladejuegos-2c3af",
+    storageBucket: "saladejuegos-2c3af.appspot.com",
+    messagingSenderId: "10732188581",
+    appId: "1:10732188581:web:910161468aae31a9b56e4c"
   }
 };
 
